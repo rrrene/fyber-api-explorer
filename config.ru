c1699b92cc3c8b928b4d3919125bae8fdf6a678e
Jan Owiesniak <owiesniak@mailbox.org>
2015-10-29 17:47:23 +0100

    Move boot process into config.ru

---
+++ config.ru
@@ -0,0 +1,3 @@
+run lambda { |e|
+  [200, {'Content-Type' => 'text/html'}, ['hello fyber!']]
+}
--- server.rb
+++
@@ -1,7 +0,0 @@
-require 'rack'
-
-Rack::Server.start(
-  :app => lambda do |e|
-    [200, {'Content-Type' => 'text/html'}, ['hello fyber!']]
-  end
-)
